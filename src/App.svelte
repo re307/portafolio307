f0778b3c66890fdff143dad57f40e8f750791ba8
Rafael Eduardo <raed307@gmail.com>
2023-04-22 23:13:17 -0600

    Portafolio-Esta es la base
    Se deja ya lista todo el cascaron para ir almacenando nueva interacción
    con esta version de menu lateral, se enfocara esfuerzo para el contenido
    dinamico en main.

--- src/App.svelte
+++ src/App.svelte
@@ -9,22 +9,36 @@
         j(e.target).next('.sub-menu').slideToggle();
         j(e.target).find('.dropdown').toggleClass('rotate');
       });
+      j('.menu-btn').click(()=>{
+        j('.sider-bar').addClass('active');
+        j('.menu-btn').class('visibility','hidden');
+      });
+      j('.close-btn').click(()=>{
+        j('.sider-bar').removeClass('active');
+        j('.menu-btn').class('visibility','visible');
+      });
     });
   const handelClickSubMenu = (e)=>{
     // e.children[0].classList.toggle('');
   }
 </script>
+<div class="menu-btn">
+  <i class="fas fa-bars"></i>
+</div>
 <div class="sider-bar">
+  <div class="close-btn">
+    <i class="fas fa-times"></i>
+  </div>
   <div class="menu">
     {#each itemesMenu as item}
       <div class="item">
         {#if item.submenu === undefined}
-          <a href="#"><i class="{item.icon}"></i>{item.modulo}</a>
+          <div class="a-item"><i class="{item.icon}"></i>{item.modulo}</div>
         {:else}
-          <a href="#" class="sub-btn"><i class="{item.icon}"></i>{item.modulo}<i class="fas fa-angle-right dropdown"></i></a>
+          <div class="sub-btn a-item"><i class="{item.icon}"></i>{item.modulo}<i class="fas fa-angle-right dropdown"></i></div>
           <div class="sub-menu">
             {#each item.submenu as submenu}
-              <a href="#" class="sub-item">{submenu.submodulo}</a>
+              <div class="a-item sub-menu-item">{submenu.submodulo}</div>
             {/each}
           </div>
         {/if}
@@ -32,3 +46,6 @@
     {/each}
   </div>
 </div>
+<section class="main">
+  <h1>Hola mundo</h1>
+</section>
