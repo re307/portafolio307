8ca2276d85d5fb98203d44816035681d9caa78a7
Rafael Eduardo <raed307@gmail.com>
2023-05-01 14:03:38 -0600

    Bucaminas_P-Listo visuales
    Ya se puede jugar el buscaminas pero falta el reinicio del juego una vez
    terminado.

--- src/lib/Buscaminas/Buscaminas.svelte
+++ src/lib/Buscaminas/Buscaminas.svelte
@@ -5,6 +5,8 @@
     let totalCeldas = largo * 18;
     let filas = new Array(totalCeldas);
     let celdasBombas = new Array();
+    let vecinasBombas = new Array();
+    let conunt = 0;
 
     let totalBombas = Math.round(totalCeldas / 8);
     for (let index = 1; index <= totalBombas; index++) {
@@ -46,6 +48,88 @@
         }
         return result
     }
+    let vecindad = (Celda)=>{
+        let vecinas;
+        if (isBorde(Celda)) {
+            if (isBordeIzquierdo(Celda)) {
+                if (isBordeSuperior(Celda)) {
+                    vecinas = [(Celda+1),(Celda+(largo+1)),Celda+largo];
+                }else
+                    if (isBordeInferior(Celda)) {
+                        vecinas = [(Celda+1),(Celda-largo),(Celda-(largo+1))];
+                    }else{
+                        vecinas = [(Celda+1),(Celda+(largo+1)),Celda+largo,(Celda-largo),((Celda-largo)+1)];
+                    }
+                
+            }else
+                if (isBordeDerecho(Celda)) {
+                    if (isBordeSuperior(Celda)) {
+                        vecinas = [(Celda+largo),((Celda+largo)-1),(Celda-1)]
+                    }else
+                        if (isBordeInferior(Celda)) {
+                            vecinas = [(Celda-1),((Celda-largo)-1),(Celda-largo)]
+                        }else{
+                            vecinas = [(Celda+largo),((Celda+largo)-1),(Celda-1),((Celda-largo)-1),(Celda-largo)]
+                        }
+                    
+                }
+        }else{
+            if (isBordeSuperior(Celda)) {
+                    vecinas = [(Celda+1),((Celda+largo)+1),(Celda+largo),((Celda+largo)-1),(Celda-1)]
+            }else
+                if (isBordeInferior(Celda)) {
+                    vecinas = [(Celda+1),(Celda-1),((Celda-largo)-1),(Celda-largo),((Celda-largo)+1)]
+                }else{
+                    vecinas = [(Celda+1),((Celda+largo)+1),(Celda+largo),((Celda+largo)-1),(Celda-1),((Celda-largo)-1),(Celda-largo),((Celda-largo)+1)]
+                }
+        }
+        if (celdasBombas.includes(Celda)) {
+            vecinasBombas[Celda]=vecinas;
+        }
+        return vecinas;
+    }
+    let ejecutaCreacion = ()=>{
+        setTimeout(()=>{
+            console.log(celdasBombas);
+            console.log(vecinasBombas);
+            vecinasBombas.forEach((vecinas)=>{
+                vecinas.forEach((vecina)=>{
+                        if (!celdasBombas.includes(vecina)) {
+                            let celdaCercania = document.getElementById(`cercania${vecina}`);
+                            // console.log(celdaCercania);
+                            let cercania = celdaCercania.innerText;
+                            let cantidad = Number(cercania);
+                            cantidad = cantidad+1;
+                            celdaCercania.innerText = `${cantidad}`;
+                            celdaCercania.style.color = determinaColor(cantidad);
+                        }
+                });
+            });
+        },1000);
+    }
+    let determinaColor = (cercania)=>{
+        let color = "";
+        switch (cercania) {
+            case 0:
+                color = "black";
+                break;
+            case 1:
+                color = "blue";
+                break;
+            case 2:
+                color = "green";
+                break;
+            case 3:
+                color = "yellow";
+                break;
+            case 4:
+                color = "red";
+                break;
+            default:
+                break;
+        }
+        return color;
+    }
 </script>
 <div class="container juego_b">
     <div class="row marcador">
@@ -57,14 +141,12 @@
                 "numero":i+1,
                 "largo":largo,
                 "ancho":totalCeldas,
-                "isBorde":isBorde(i+1),
-                "izquierdo":isBordeIzquierdo(i+1),
-                "derecho":isBordeDerecho(i+1),
-                "superior":isBordeSuperior(i+1),
-                "inferior":isBordeInferior(i+1),
-                "isBomba":celdasBombas.includes(i+1)
+                "vecinas":vecindad(i+1),
+                "isBomba":celdasBombas.includes(i+1),
+                "bombas":celdasBombas
             }}>
             </Celda>
         {/each}
     </div>
+    {ejecutaCreacion()}
 </div>
--- src/lib/Buscaminas/Celda.svelte
+++ src/lib/Buscaminas/Celda.svelte
@@ -1,76 +1,33 @@
 <script>
+    import boom from '../../assets/boom.png'
     export let data;
-    let vecinas;
+    let vecinas = data.vecinas;
     let liberada = false;
     let cercania = 0;
-    if (data.isBorde) {
-        if (data.izquierdo) {
-            if (data.superior) {
-                vecinas = [(data.numero+1),(data.numero+(data.largo+1)),data.numero+data.largo];
-            }else
-                if (data.inferior) {
-                    vecinas = [(data.numero+1),(data.numero-data.largo),(data.numero-(data.largo+1))];
-                }else{
-                    vecinas = [(data.numero+1),(data.numero+(data.largo+1)),data.numero+data.largo,(data.numero-data.largo),((data.numero-data.largo)+1)];
-                }
-            
-        }else
-            if (data.derecho) {
-                if (data.superior) {
-                    vecinas = [(data.numero+data.largo),((data.numero+data.largo)-1),(data.numero-1)]
-                }else
-                    if (data.inferior) {
-                        vecinas = [(data.numero-1),((data.numero-data.largo)-1),(data.numero-data.largo)]
-                    }else{
-                        vecinas = [(data.numero+data.largo),((data.numero+data.largo)-1),(data.numero-1),((data.numero-data.largo)-1),(data.numero-data.largo)]
-                    }
-                
-            }
-    }else{
-        if (data.superior) {
-                vecinas = [(data.numero+1),((data.numero+data.largo)+1),(data.numero+data.largo),((data.numero+data.largo)-1),(data.numero-1)]
-        }else
-            if (data.inferior) {
-                vecinas = [(data.numero+1),(data.numero-1),((data.numero-data.largo)-1),(data.numero-data.largo),((data.numero-data.largo)+1)]
-            }else{
-                vecinas = [(data.numero+1),((data.numero+data.largo)+1),(data.numero+data.largo),((data.numero+data.largo)-1),(data.numero-1),((data.numero-data.largo)-1),(data.numero-data.largo),((data.numero-data.largo)+1)]
-            }
-    }
-    if (data.isBomba) {
-        vecinas.forEach((vecina)=>{
-            setTimeout(()=>{
-                let celdaCercania = document.getElementById(`cercania${vecina}`);
-                let cercania = celdaCercania.innerText;
-                let cantidad = Number(cercania);
-                celdaCercania.innerText = `${cantidad++}`;
-            },1000);
-        });
-    }
     let destapaVecinas = ()=>{
-        if (!liberada) {
+        if (!liberada&&!data.isBomba) {
             let celda = document.getElementById(`celda${data.numero}`);
             celda.className = "celda_libre";
-            // celda.childNodes[0].style.display = "block";
+            let hijo = celda.childNodes[0].childNodes[0];
+            // console.log(hijo);
             liberada = true;
-            vecinas.forEach(element => {
-                // console.log(element)
+            if (hijo.innerText == "") {
+                vecinas.forEach(element => {
+                    setTimeout(()=>{
+                        document.getElementById(`celda${element}`).click();
+                    },1);
+                });
+            }
+        }
+        if (data.isBomba) {
+            data.bombas.forEach(element => {
+                let celda = document.getElementById(`celda${data.numero}`);
+                celda.className = "celda_libre";
+                liberada = true;
                 setTimeout(()=>{
                     document.getElementById(`celda${element}`).click();
                 },1);
             });
-        }
-    }
-    let determinaColor = ()=>{
-        let color = "";
-        switch (cercania) {
-            case 0:
-                color = "black";
-                break;
-            case 1:
-                color = "blue";
-                break;
-            default:
-                break;
         }
     }
 </script>
@@ -80,9 +37,9 @@
 >
     <div style="display: {liberada?'block':'none'};">
         {#if data.isBomba}
-            <img class="logo" alt="Vite logo" />
+            <img src="{boom}" class="logo" alt="Vite logo" />
         {:else}
-            <p id="cercania{data.numero}">{cercania}</p>
+            <p id="cercania{data.numero}">{cercania>0?cercania:""}</p>
         {/if}
     </div>
 </div>
