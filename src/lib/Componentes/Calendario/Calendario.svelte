71c2e2846d1af974d9fdb5747d276f1d746b67aa
Rafael Eduardo <raed307@gmail.com>
2023-05-28 09:49:02 -0600

    Mes y Año
    Se deja lista la logica de implementacion de impresion de meses sin el
    cambio dinamico y se deja parte de la logica para el pintado de los
    años.

--- src/lib/Componentes/Calendario/Calendario.svelte
+++ src/lib/Componentes/Calendario/Calendario.svelte
@@ -1,86 +1,56 @@
 <script>
     import './calendario.css'
+    import Celda from './Celda.svelte'
     const fechaActual = new Date();
+    let infoImprimr = new Array();
+    let construye = 2;
     let htmlInicial = ``;
     let mesInicial = fechaActual.getMonth()+1;
     let mes = fechaActual.getMonth()+1;
     let ano = fechaActual.getFullYear();
-
-    let todoCalendario = ()=>{
-        let díaUnoAnual = new Date(`${fechaActual.getUTCFullYear()}-01-01 00:00:00`);
-        let nuevoGrid = '';
-        let actDesAct = 'mesDesActivo';
-        let styledes ='style="width: 5px;height: 5px;"';
-        for (let ai = 1; ai < 12; ai++) {
-            let diainicio = new Date(`${fechaActual.getUTCFullYear()}-${ai}-01 00:00:00`);
-            if (ai>1) {
-                diainicio.setDate(diainicio.getDate()-diainicio.getDay());
-            }
-            nuevoGrid = nuevoGrid+`
-                <div class="col-sm-1 gridDSMA ${actDesAct}"${styledes}>
-            <div class="dia">do.</div>
-            <div class="dia">lu.</div>
-            <div class="dia">ma.</div>
-            <div class="dia">mi.</div>
-            <div class="dia">ju.</div>
-            <div class="dia">vi.</div>
-            <div class="dia">sa.</div>
-            `;
-            for (let index = 1; index < 43; index++) {
-                nuevoGrid =nuevoGrid+`
-                    <div class="${diainicio.toDateString()==fechaActual.toDateString()?'actual':'celda'}">${diainicio.getDate()}</div>
-                `;
-                diainicio.setDate(diainicio.getDate()+1);
-            }
-            nuevoGrid = nuevoGrid+`</div>`;
-            if (diainicio.getMonth()==fechaActual.getMonth()) {
-                actDesAct = 'mesActivo';
-                styledes='';
-            }else{actDesAct = 'mesDesActivo';styledes ='style="width: 5px;height: 5px;"';}
-            
-        }
-        // console.log('nuevoGrid: ',nuevoGrid);
-        htmlInicial = nuevoGrid;
-    }
-    let mesVista = (mesVisual)=>{
-        console.log('mesVisual: ',mesVisual);
-        htmlInicial ='';
-        let mesPintar ;
-        let mesNumero = mesVisual;
-        if (mesNumero == 13) {
-            mesNumero = 1;
+    let mesArray = (mesVisual_A)=>{
+        let mesPintar_A;
+        let mesNumero_A = mesVisual_A;
+        if (mesNumero_A == 13) {
+            mesNumero_A = 1;
             mesInicial = 1;
             ano = ano + 1 ;
         }
-        if (mesNumero == 0) {
-            mesNumero = 12;
+        if (mesNumero_A == 0) {
+            mesNumero_A = 12;
             mesInicial = 12;
             ano=ano-1;
         }
-        mesPintar = new Date(`${ano}-${mesNumero}-01 00:00:00`);
-        console.log(`${ano}-${mesNumero}-01 00:00:00`);
-        mesPintar.setDate(mesPintar.getDate()-mesPintar.getDay());
-        console.log(mesPintar.toString());
-        let mesGrid = `
-                <div class="gridDSMA col-sm-12">
-            <div class="dia">do.</div>
-            <div class="dia">lu.</div>
-            <div class="dia">ma.</div>
-            <div class="dia">mi.</div>
-            <div class="dia">ju.</div>
-            <div class="dia">vi.</div>
-            <div class="dia">sa.</div>
-            `;
-        for (let index = 1; index < 43; index++) {
-            mesGrid =mesGrid+`
-                <div class="${mesPintar.toDateString()==fechaActual.toDateString()?'actual':'celda'}">${mesPintar.getDate()}</div>
-            `;
-            mesPintar.setDate(mesPintar.getDate()+1);
+        mesPintar_A = new Date(`${ano}-${mesNumero_A}-01 00:00:00`);
+        mesPintar_A.setDate(mesPintar_A.getDate()-mesPintar_A.getDay());
+        for (let index = 0; index < 42; index++) {
+            infoImprimr.push(mesPintar_A.toString());
+            mesPintar_A.setDate(mesPintar_A.getDate()+1);
         }
-        mesGrid = mesGrid+`</div>`;
-        htmlInicial = mesGrid;
     }
-    mesVista(mes);
+    let mesesArray = (ano)=>{
+        for (let index = 0; index < 12; index++) {
+            let infoMes = {
+                "ano":ano,
+                "mes":index
+            }
+            infoImprimr.push(infoMes);
+        }
+    }
+    let anoArray = (ano)=>{
+        let anoS = `${ano}`;
+        let disminucion = Number(anoS[anoS.length-1])+1;
+        let inicio = ano - disminucion;
+        for (let index = inicio; index < (inicio+10); index++) {
+            
+        }
+    }
+    if (construye === 1) {
+        mesArray(mes);
+    }
+    if (construye === 2) {
+        mesesArray(ano);
+    }
 </script>
 <div class="container sinpadding">
     <div class="row">
@@ -89,6 +59,32 @@
         <div class="col-sm-4 siguiente btn-cand" on:click={()=>{mesInicial=mesInicial+1;mesVista(mesInicial)}}>Siguiente</div>
     </div>
     <div class="row">
-        {@html htmlInicial}
+        {#if construye === 1}
+            <div class="gridMes col-sm-12">
+                <div class="dia">do.</div>
+                <div class="dia">lu.</div>
+                <div class="dia">ma.</div>
+                <div class="dia">mi.</div>
+                <div class="dia">ju.</div>
+                <div class="dia">vi.</div>
+                <div class="dia">sa.</div>
+                {#each infoImprimr as data,i }
+                    <Celda info={{
+                        "seleccionado":construye,
+                        "imprime":data
+                    }}></Celda>
+                {/each}
+            </div>
+        {/if}
+        {#if construye === 2}
+            <div class="gridMesesAno col-sm-12">
+                {#each infoImprimr as data,i }
+                    <Celda info={{
+                        "seleccionado":construye,
+                        "imprime":data
+                    }}></Celda>
+                {/each}
+            </div>
+        {/if}
     </div>
 </div>
---
+++ src/lib/Componentes/Calendario/Celda.svelte
@@ -0,0 +1,35 @@
+<script>
+
+    export let info;
+
+    const hoy = new Date();
+    const mesActual = hoy.getMonth();
+    const ano = hoy.getFullYear();
+    const mesesNombre = ["ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC"];
+    let fechaIngresada = new Date(info.imprime);
+    let styleS = null;
+    let htmlInsert = null;
+    switch (info.seleccionado) {
+        case 1:
+            if (fechaIngresada.toDateString() === hoy.toDateString()) {
+                styleS = "dia_actual";
+            }else{
+                styleS = "dia_libre";
+            }
+            htmlInsert = `<div class="${styleS}">${fechaIngresada.getDate()}</div>`
+            break;
+        case 2:
+            if (mesActual === info.imprime.mes&&ano ===info.imprime.ano) {
+                styleS = "mesAno_actual";
+            }else{
+                styleS = "mesAno_libre";
+            }
+            htmlInsert = `<div class="${styleS}"><p>${mesesNombre[info.imprime.mes]}</p></div>`
+            break;
+    
+        default:
+            break;
+    }
+
+</script>
+{@html htmlInsert}
